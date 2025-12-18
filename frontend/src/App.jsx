65eaec2aa16b23837f91124eb5575caa34f73888
MiradoAndrian04 <miradoandrian25@gmail.com>
2025-12-18 18:08:27 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen text-black flex items-center justify-center">
+      <h1 className="text-5xl font-semibold">
+        Tailwind OK 🚀
+      </h1>
+    </div>
+  )
+}
